6f6c46e9840dcfbb9f7a72e73c4d3f5c14b1770b
WNagorny <walery.nagorny@gmail.com>
2023-05-30 20:29:49 +0200

    Changed media queries and fixed all bugs into the all files. Added Animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 let menu =document.querySelector('#menu-icon');
 let navbar =document.querySelector('.navbar');
 
@@ -12,3 +13,20 @@ const removeclasses = () => {
 
 window.addEventListener('scroll', removeclasses)
 menu.addEventListener('click', toggleMenu)
+
+const sr = ScrollReveal ({
+   distance: '60px',
+   duration: 2500,
+   delay: 300,
+   reset: true
+})
+
+sr.reveal('.text', {delay:200, origin: 'top'})
+sr.reveal('.form-container form', {delay:300, origin: 'left'})
+sr.reveal('.headnig', {delay:300, origin: 'top'})
+
+sr.reveal('.ride-container .box', {delay:200, origin: 'top'})
+sr.reveal('.services-container .box', {delay:200, origin: 'top'})
+sr.reveal('.about-container .box', {delay:200, origin: 'top'})
+sr.reveal('.reviews-container', {delay:400, origin: 'top'})
+sr.reveal('.newsletter .box', {delay:400, origin: 'bottom'})
